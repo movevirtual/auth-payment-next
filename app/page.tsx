eeedd66633b0e49e975202b2f74db4d8f8c54150
Arghya Das <github@movevirtual.co>
2023-07-11 23:34:25 +0530

    auth-payment edited

--- app/page.tsx
+++ app/page.tsx
@@ -2,7 +2,7 @@ import Image from "next/image";
 
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
+    <main className="flex flex-col items-center justify-between p-24">
       <div className="z-10 w-full max-w-5xl mx-auto items-center justify-center text-sm lg:flex">
         <h2 className="text-8xl font-bold tracking-tight text-center bg-gradient-to-tr from-black via-slate-800 to-neutral-600 dark:from-white dark:via-slate-200 dark:to-neutral-300 text-transparent bg-clip-text">
           Document Signing <br /> in one Place
