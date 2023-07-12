fda9c384ede8812a5ec17087fddc1df20223f510
Arghya Das <github@movevirtual.co>
2023-07-12 13:33:48 +0530

    auth-payment edited

--- app/api/users/route.ts
+++ app/api/users/route.ts
@@ -1,7 +1,7 @@
 import { Webhook } from "svix";
 import { headers } from "next/headers";
 import { NextResponse } from "next/server";
-const webhookSecret: string = process.env.WEBHOOK_SECRET || "";
+const webhookSecret: string = process.env.CLERK_WEBHOOK_SECRET || "";
 
 export async function POST(req: any) {
   const payload = await req.json();
