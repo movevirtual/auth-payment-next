c31f3dec62b4cd083be32b831048376861ac6d5d
Arghya Das <github@movevirtual.co>
2023-07-12 14:13:24 +0530

    auth-payment edited

--- app/api/users/route.ts
+++ app/api/users/route.ts
@@ -4,6 +4,37 @@ import { NextResponse } from "next/server";
 const webhookSecret: string = process.env.CLERK_WEBHOOK_SECRET || "";
 
 export async function POST(req: any) {
-  const headers = req.headers;
-  console.log(headers);
+  const payload = await req.json();
+  const payloadString = JSON.stringify(payload);
+  const svixId = req.headers.get("svix-id");
+  const svixIdTimeStamp = req.headers.get("svix-timestamp");
+  const svixSignature = req.headers.get("svix-signature");
+  if (!svixId || !svixIdTimeStamp || !svixSignature) {
+    console.log("svixId", svixId);
+    console.log("svixIdTimeStamp", svixIdTimeStamp);
+    console.log("svixSignature", svixSignature);
+    return new Response("Error occured", {
+      status: 400,
+    });
+  }
+  const svixHeaders = {
+    "svix-id": svixId,
+    "svix-timestamp": svixIdTimeStamp,
+    "svix-signature": svixSignature,
+  };
+  const wh = new Webhook(webhookSecret);
+  let evt: Event | null = null;
+  try {
+    evt = wh.verify(payloadString, svixHeaders) as Event;
+    console.log("Successfully verified event");
+    console.log("Event: ", evt);
+    return new Response("Success", {
+      status: 200,
+    });
+  } catch (_) {
+    console.log("error");
+    return new Response("Error occured", {
+      status: 400,
+    });
+  }
 }
