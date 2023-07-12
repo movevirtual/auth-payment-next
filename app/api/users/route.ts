ded43ec84bf3e7d159139bf86ef61a0b087a770f
Arghya Das <github@movevirtual.co>
2023-07-12 13:31:15 +0530

    auth-payment edited

--- app/api/users/route.ts
+++ app/api/users/route.ts
@@ -1,35 +1,38 @@
-import { NextApiRequest } from "next";
-import { NextResponse } from "next/server";
 import { Webhook } from "svix";
+import { headers } from "next/headers";
+import { NextResponse } from "next/server";
+const webhookSecret: string = process.env.WEBHOOK_SECRET || "";
 
-export const runtime = "nodejs";
-
-export async function GET() {
-  return NextResponse.json(
-    { Success: "You are in the wrong direction 😉" },
-    { status: 200 }
-  );
-}
-
-export const config = {
-  api: {
-    bodyParser: false,
-  },
-};
-
-const secret: string | undefined = process.env.CLERK_WEBHOOK_SECRET;
-
-export async function POST(req: NextApiRequest) {
-  const payload: any = await req.body;
-  const headers: any = req.headers;
-  console.log(payload);
-  console.log(headers);
-  const wh: Webhook = new Webhook(secret || "");
-  let event: any;
+export async function POST(req: any) {
+  const payload = await req.json();
+  const payloadString = JSON.stringify(payload);
+  const headerPayload = headers();
+  const svixId = headerPayload.get("svix-id") || "";
+  const svixIdTimeStamp = headerPayload.get("svix-timestamp") || "";
+  const svixSignature = headerPayload.get("svix-signature") || "";
+  if (!svixId || !svixIdTimeStamp || !svixSignature) {
+    console.log("svixId", svixId);
+    console.log("svixIdTimeStamp", svixIdTimeStamp);
+    console.log("svixSignature", svixSignature);
+    return new Response("Error occured", {
+      status: 400,
+    });
+  }
+  const svixHeaders = {
+    "svix-id": svixId,
+    "svix-timestamp": svixIdTimeStamp,
+    "svix-signature": svixSignature,
+  };
+  const wh = new Webhook(webhookSecret);
+  let evt: Event | null = null;
   try {
-    event = wh.verify(payload, headers);
-    console.log(event);
-  } catch (err) {
-    NextResponse.json({ Error: "Invalid webhook signature" }, { status: 400 });
+    evt = wh.verify(payloadString, svixHeaders) as Event;
+    console.log("Successfully verified event");
+    console.log("Event: ", evt);
+  } catch (_) {
+    console.log("error");
+    return new Response("Error occured", {
+      status: 400,
+    });
   }
 }
