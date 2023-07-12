f723c4d97713f6d159f2719af9f75d530c288a81
Arghya Das <github@movevirtual.co>
2023-07-12 12:30:43 +0530

    auth-payment edited

--- app/api/users/route.ts
+++ app/api/users/route.ts
@@ -1,5 +1,7 @@
 import { NextResponse } from "next/server";
 import { Webhook } from "svix";
+import { buffer } from "stream/consumers";
+import { NextApiRequest, NextApiResponse } from "next";
 
 export async function GET() {
   return NextResponse.json(
@@ -9,28 +11,6 @@ export async function GET() {
 }
 
 export async function POST(req: Request) {
-  const payload: any = req.body;
-  console.log("Payload: " + JSON.stringify(payload));
-  const headers: any = req.headers;
-  console.log("Headers: " + JSON.stringify(headers));
-  const secret: string | undefined = process.env.CLERK_WEBHOOK_SECRET;
-  const wh: Webhook = new Webhook(secret || "");
-  let event: any;
-  try {
-    event = wh.verify(payload, headers);
-    if (event.type === "user.created") {
-      const userEmailAdress: string =
-        event.data.email_addresses[0].email_address;
-      console.log("User created: " + userEmailAdress);
-      return NextResponse.json({ error: "User created" }, { status: 200 });
-    } else {
-      return NextResponse.json({ error: "User not created" }, { status: 500 });
-    }
-  } catch (err: any) {
-    console.error(err);
-    return NextResponse.json(
-      { error: "Internal Server Error" },
-      { status: 500 }
-    );
-  }
+  const body = await req.json();
+  console.log(body);
 }
